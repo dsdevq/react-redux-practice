166dc07fe795d4cdf929996f7d8ea09ca615cbd0
Denis <payar3282@gmail.com>
2022-08-18 17:24:28 +0300

    remove reducers import

--- src/featured/users/usersSlice.js
+++ src/featured/users/usersSlice.js
@@ -74,7 +74,6 @@ export const usersSlice = createSlice({
 })
 
 //!!!! Action creators are generated for each case reducer function
-export const { create, deleteUser } = usersSlice.actions
 
 export const selectAllUsers = (state) => state.users.users
 
